e431c0572453aa60f31466b94bd4753982103e6d
niketaj <niketa.jain@weboniselab.com>
2018-03-14 13:10:11 +0530

    wordy file

--- wordy/wordy.go
+++ wordy/wordy.go
@@ -7,66 +7,105 @@ import ("math"
 
 )
 
-func Wordy(case1 string) int {
+func Wordy(input string) int {
 
-//case1 := "What is 5 plus 13?"
-//case1 := "What is 53 plus 2?"
+
 
 re := regexp.MustCompile("[0-9]+")
-   operands := re.FindAllString(case1, -1)
+operands := re.FindAllString(input, -1)
 
 op1, _ := strconv.Atoi(operands[0])
 op2, _ := strconv.Atoi(operands[1])
 
+
    if len(operands) == 2 {
 
-if strings.Contains(case1,"plus") {
-evalval := op1 + op2
+      if strings.Contains(input,"plus") {
+
+      evalval := op1 + op2
    	return evalval
-   } else if strings.Contains(case1,"minus"){
+
+   } else if strings.Contains(input,"minus"){
+
    	minus_ans := op1 - op2
    	return minus_ans
-   } else if strings.Contains(case1,"multiplied") {
-   	multiply_ans := op1 * op2 
+
+   } else if strings.Contains(input,"multiplied") {
+   	
+      multiply_ans := op1 * op2 
    	return multiply_ans
-   } else if strings.Contains(case1,"divided") {
-   	divide_ans := op1 / op2
+
+   } else if strings.Contains(input,"divided") {
+   	
+
+      divide_ans := op1 / op2
    	return divide_ans
+
   
-   } else if strings.Contains(case1,"power") {
-   	raised_to := math.Pow(float64(op1),float64(op2))
+   } else if strings.Contains(input,"power") {
+   	
+
+      raised_to := math.Pow(float64(op1),float64(op2))
    	return int(raised_to)
+
+
    }
 } else if len(operands) == 3 {
-op3, _ := strconv.Atoi(operands[2])
 
-var final_eval int
+   op3, _ := strconv.Atoi(operands[2])
 
-//solve left part
-if strings.Contains(case1,"plus") {
-case1 = strings.Replace(case1,"plus","",1)
-final_eval = op1 + op2
-   } else if strings.Contains(case1,"minus"){
-   	case1 = strings.Replace(case1,"minus","",1)
+   var final_eval int
+
+
+   if strings.Contains(input,"plus") {
+
+         input = strings.Replace(input,"plus","",1)
+         final_eval = op1 + op2
+
+
+   } else if strings.Contains(input,"minus"){
+
+
+   	input = strings.Replace(input,"minus","",1)
    	final_eval = op1-op2
-   } else if strings.Contains(case1,"multiplied") {
-   	case1 = strings.Replace(case1,"multiplied","",1)
+
+
+   } else if strings.Contains(input,"multiplied") {
+   	
+
+      input = strings.Replace(input,"multiplied","",1)
    	final_eval = op1 * op2 
-   } else if strings.Contains(case1,"divided") {
-   	case1 = strings.Replace(case1,"divided","",1)
+
+
+   } else if strings.Contains(input,"divided") {
+   	
+
+      input = strings.Replace(input,"divided","",1)
    	final_eval = op1 / op2
+
+
    }
    
-   if strings.Contains(case1,"plus") {
-final_eval = final_eval + op3
-   } else if strings.Contains(case1,"minus"){
-   	final_eval = final_eval - op3
-   } else if strings.Contains(case1,"multiplied") {
-   	final_eval = final_eval * op3 
-   } else if strings.Contains(case1,"divided") {
-   	final_eval = final_eval / op3
+   if strings.Contains(input,"plus") {
+      
+      final_eval = final_eval + op3
+
+   } else if strings.Contains(input,"minus"){
+   	
+      final_eval = final_eval - op3
+
+   } else if strings.Contains(input,"multiplied") {
+   	
+      final_eval = final_eval * op3 
+
+   } else if strings.Contains(input,"divided") {
+   	
+      final_eval = final_eval / op3
    }
+
    return final_eval
 }
+
 return 0
+
 }
--- wordy/wordy_test.go
+++ wordy/wordy_test.go
@@ -6,18 +6,82 @@ var wordyTests = []struct {
 	n string
 	expected int
 }{
-	{"what is 5 plus 13?",18},
-	{"what is 53 plus 2?",55},
-	//{"What is -1 plus -10?",-11},
-	{"What is 123 plus 5?",128},
-	{"What is 10 minus 2?",8},
-	{"What is 33 multiplied by 2?",66},
-	{"what is 10 divided by 2?",5},
-	{"what is 5 plus 10 plus 2?",17},
-	{"What is 5 plus 2 minus 1?",6},
-	{"What is 5 power to 2?",25},
-	{"What is 20 minus 4 minus 13?",3},
+	{
+		"what is 5 plus 13?",18,
+	},
 
+
+	{
+		"what is 53 plus 2?",55,
+	},
+
+
+	{
+		"What is 123 plus 5?",128,
+	},
+
+
+	{
+		"What is 10 minus 2?",8,
+	},
+
+
+	{
+		"What is 123 plus 45678?",45801,
+	},
+
+
+	{
+		"What is 33 multiplied by 2?",66,
+	},
+
+
+	{
+		"what is 10 divided by 2?",5,
+	},
+
+
+	{
+		"what is 5 plus 10 plus 2?",17,
+	},
+
+
+	{
+		"What is 5 plus 2 minus 1?",6,
+	},
+
+
+	{
+		"What is 5 power to 2?",25,
+	},
+
+
+	{
+		"What is 20 minus 4 minus 13?",3,
+	},
+
+
+	{
+		"What is 4 minus -12?",-8,
+	},
+
+
+	{
+		"What is 1 plus 1 plus 1?",3,
+	},
+
+
+	{
+		"What is 1 plus 5 minus -2?",4,
+	},
+
+
+	{
+		"What is 20 minus 4 minus 13?",3,
+	},
+	{
+		"What is 17 minus 6 plus 3?",14,//error in this input
+	},
 
 }
 
