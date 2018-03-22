7b5062a1475d51b9ace4e305782630337f6e9572
niketaj <niketa.jain@weboniselab.com>
2018-03-22 12:21:50 +0530

    api-assgn folder contains one file name simple-getrest-ex.go.This file performs simple GET Request using open standard library

---
+++ api-assgn/simple-getrest-ex.go
@@ -0,0 +1,25 @@
+package main
+
+import ("fmt"
+		"log"
+		"net/http")
+
+func homePage(w http.ResponseWriter,r *http.Request) {
+
+	fmt.Fprintf(w,"homepage end url")
+}
+func allBooks(w http.ResponseWriter,r *http.Request){
+	fmt.Fprintf(w,"Get request on books")
+}
+
+
+func handleRequests(){
+
+	http.HandleFunc("/",homePage)
+	http.HandleFunc("/books",allBooks)
+	log.Fatal(http.ListenAndServe(":8084",nil))
+}
+func main() {
+
+	handleRequests()
+}
